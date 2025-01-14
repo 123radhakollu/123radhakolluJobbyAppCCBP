b5624cd8e4a67570ccc3e2919dc69edf9f9b2b73
Kollu <kolluradha66@gmail.com>
2025-01-14 06:48:15 +0000

    solution

--- src/components/JobItem/index.js
+++ src/components/JobItem/index.js
@@ -1,57 +1,55 @@
 import {Link} from 'react-router-dom'
+import {BsFillBriefcaseFill, BsStarFill} from 'react-icons/bs'
 import {MdLocationOn} from 'react-icons/md'
-import {AiFillStar} from 'react-icons/ai'
 import './index.css'
+
 const JobItem = props => {
   const {jobData} = props
   const {
     companyLogoUrl,
     employmentType,
-    id,
     jobDescription,
     location,
     packagePerAnnum,
     rating,
     title,
+    id,
   } = jobData
   return (
     <Link to={`/jobs/${id}`} className="link-item">
-      <li className="job-item-container">
-        <div className="first-part-container">
-          <div className="img-title-container">
+      <li className="job-item">
+        <div className="logo-title-location-container">
+          <div className="logo-title-container">
             <img
-              className="company-logo"
               src={companyLogoUrl}
               alt="company logo"
+              className="company-logo"
             />
             <div className="title-rating-container">
               <h1 className="title-heading">{title}</h1>
-              <div className="star-rating-container">
-                <AiFillStar className="star-icon" />
-                <p className="rating-text">{rating}</p>
+              <div className="rating-container">
+                <BsStarFill className="rating-icon" />
+                <p className="rating-heading">{rating}</p>
               </div>
             </div>
           </div>
           <div className="location-package-container">
-            <div className="location-job-type-container">
-              <div className="location-icon-location-container">
+            <div className="location-employee-container">
+              <div className="location-container">
                 <MdLocationOn className="location-icon" />
-                <p className="location">{location}</p>
+                <p className="location-heading">{location}</p>
               </div>
-              <div className="employment-type-icon-employment-type-container">
-                <p className="job-type">{employmentType}</p>
+              <div className="employee-type-container">
+                <BsFillBriefcaseFill className="brief-case-icon" />
+                <p className="employee-type-heading">{employmentType}</p>
               </div>
             </div>
-            <div>
-              <p className="package">{packagePerAnnum}</p>
-            </div>
+            <p className="package-heading">{packagePerAnnum}</p>
           </div>
         </div>
-        <hr className="item-hr-line" />
-        <div className="second-part-container">
-          <h1 className="description-heading">Description</h1>
-          <p className="description-para">{jobDescription}</p>
-        </div>
+        <hr className="line" />
+        <h1 className="description-heading">Description</h1>
+        <p className="description-text">{jobDescription}</p>
       </li>
     </Link>
   )
--- src/components/SimilarJobs/index.js
+++ src/components/SimilarJobs/index.js
@@ -1,49 +1,48 @@
+import {BsFillBriefcaseFill, BsStarFill} from 'react-icons/bs'
 import {MdLocationOn} from 'react-icons/md'
-import {AiFillStar} from 'react-icons/ai'
 import './index.css'
-const SimilarJobs = props => {
-  const {similarJobData} = props
+
+const SimilarJobItem = props => {
+  const {jobDetails} = props
   const {
     companyLogoUrl,
-    // eslint-disable-next-line no-unused-vars
-    id,
-    jobDescription,
     employmentType,
+    jobDescription,
     location,
     rating,
     title,
-  } = similarJobData
-  console.log(similarJobData)
+  } = jobDetails
   return (
-    <li className="similar-job-li-container">
-      <div className="img-job-title-container">
-        <img
-          className="company-job-logo"
-          src={companyLogoUrl}
-          alt="similar job company logo"
-        />
-        <div className="title-job-rating-container">
-          <h1 className="title-job-heading">{title}</h1>
-          <div className="star-job-rating-container">
-            <AiFillStar className="star-job-icon" />
-            <p className="rating-job-text">{rating}</p>
+    <li className="similar-job-item">
+      <div className="logo-title-location-container">
+        <div className="logo-title-container">
+          <img
+            src={companyLogoUrl}
+            alt="similar job company logo"
+            className="company-logo"
+          />
+          <div className="title-rating-container">
+            <h1 className="title-heading">{title}</h1>
+            <div className="rating-container">
+              <BsStarFill className="rating-icon" />
+              <p className="rating-heading">{rating}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="second-part-job-container">
-        <h1 className="description-job-heading">Description</h1>
-        <p className="description-job-para">{jobDescription}</p>
-      </div>
-      <div className="location-job-details-type-container">
-        <div className="location-job-icon-location-container">
-          <MdLocationOn className="location-job-icon" />
-          <p className="location-job">{location}</p>
-        </div>
-        <div className="employment-job-type-icon-employment-type-container">
-          <p className="job-type">{employmentType}</p>
+        <h1 className="description-heading">Description</h1>
+        <p className="description-text">{jobDescription}</p>
+        <div className="location-employee-container">
+          <div className="location-container">
+            <MdLocationOn className="location-icon" />
+            <p className="location-heading">{location}</p>
+          </div>
+          <div className="employee-type-container">
+            <BsFillBriefcaseFill className="brief-case-icon" />
+            <p className="employee-type-heading">{employmentType}</p>
+          </div>
         </div>
       </div>
     </li>
   )
 }
-export default SimilarJobs
+export default SimilarJobItem
